7eb3745c13b1eabda881ff8590c89868be862f1b
leleking <simeonnortey@gmail.com>
2019-10-23 23:53:17 +0000

    evn

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        this is welcome to vue-anchor with Boss Jerry
+        this is welcome to vue-anchor with {{this.name}}
         <router-link to="/" exact-active-class="active">welcome</router-link>
         <router-link to="/about">about</router-link>
     </div>
@@ -8,7 +8,11 @@
 
 <script>
     export default {
-        
+        data() {
+            return {
+                name: process.env.VUE_APP_NAME
+            }
+        },
     }
 </script>
 
