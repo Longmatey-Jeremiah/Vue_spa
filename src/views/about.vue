32cf73e32a3b47fe7fe397a63b831572feffd743
Longmatey-Jeremiah <longmateyjeremiah1@gmail.com>
2019-10-23 23:17:01 +0000

    active

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         this is our about component :)
+        <router-link to="/">welcome</router-link>
+        <router-link to="/about" exact-active-class="active">about</router-link>
     </div>
 </template>
 
